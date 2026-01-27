ca704938f577f74c12873ef5dfccc9c96de60a7f
Friedrich Stoltzfus <friedrichstoltzfus@gmail.com>
2026-01-27 15:20:47 -0500

    fix: remove the completion style setting
    Users can change this in their settings. I didn't realize that when I
    first started this project.

--- crates/nu-protocol/src/config/mod.rs
+++ crates/nu-protocol/src/config/mod.rs
@@ -9,7 +9,7 @@ use std::collections::HashMap;
 pub use ansi_coloring::UseAnsiColoring;
 pub use clip::ClipConfig;
 pub use completions::{
-    CompletionAlgorithm, CompletionConfig, CompletionSort, CompletionStyle, ExternalCompleterConfig,
+    CompletionAlgorithm, CompletionConfig, CompletionSort, ExternalCompleterConfig,
 };
 pub use datetime_format::DatetimeFormatConfig;
 pub use display_errors::DisplayErrors;
